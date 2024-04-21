5963bd040f271cc0349606c5111f67b587482610
kimhyeok <kimmang940715@naver.com>
2024-04-22 01:01:27 +0900

    feat : 상수변경

--- My project/Assets/@Scripts/PlayerController.cs
+++ My project/Assets/@Scripts/PlayerController.cs
@@ -20,19 +20,19 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)) 
         {
-            transform.position += new Vector3(0, 0, 1.0f) * Time.deltaTime * _speed;
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
         }
         if (Input.GetKey(KeyCode.S)) 
         {
-            transform.position -= new Vector3(0, 0, 1.0f) * Time.deltaTime * _speed;
+            transform.position += Vector3.back * Time.deltaTime * _speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(1, 0, 0.0f) * Time.deltaTime * _speed;
+            transform.position += Vector3.left * Time.deltaTime * _speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(1, 0, 0.0f) * Time.deltaTime * _speed;
+            transform.position += Vector3.right * Time.deltaTime * _speed;
         }
     }
 }
