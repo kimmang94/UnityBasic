2dee3c2c2c42c5b90db791daef18de5024b3677f
kimhyeok <kimmang940715@naver.com>
2024-04-21 21:48:08 +0900

    feat : 프레임당 속도를 조정하는방법

--- My project/Assets/@Scripts/PlayerController.cs
+++ My project/Assets/@Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float _speed = 10f;
     // GameObject (Player)
         // Transform
         // PlayerController
@@ -19,19 +20,19 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)) 
         {
-            transform.position += new Vector3(0, 0, 1.0f);
+            transform.position += new Vector3(0, 0, 1.0f) * Time.deltaTime * _speed;
         }
         if (Input.GetKey(KeyCode.S)) 
         {
-            transform.position -= new Vector3(0, 0, 1.0f);
+            transform.position -= new Vector3(0, 0, 1.0f) * Time.deltaTime * _speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(1, 0, 0.0f);
+            transform.position -= new Vector3(1, 0, 0.0f) * Time.deltaTime * _speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(1, 0, 0.0f);
+            transform.position += new Vector3(1, 0, 0.0f) * Time.deltaTime * _speed;
         }
     }
 }
